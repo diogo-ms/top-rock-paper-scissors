81cde51316243eb25ba8c086ee9757be94bcc4d1
diogo-ms <diogomarquessimoes@gmail.com>
2023-04-30 01:41:50 +0200

    completed the if statement when the computer choice is rock

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 function playRound(playerSelection, computerSelection) {
+    let gameOverMessage;
     playerSelection = playerSelection.toLowerCase()
-    return playerSelection;
+    if (computerSelection == "rock") {
+        switch (playerSelection) {
+            case "rock":
+                gameOverMessage = "It's a Tie !";
+                break;
+            case "scissors":
+                gameOverMessage = "You lost ! The computer wins this round !";
+                break;
+            case "paper":
+                gameOverMessage = "You defeated the computer !";
+                break;
+        }
+    }
+    return gameOverMessage;
 }
 function getComputerChoice() {
     let arrGameItems = ["rock", "paper", "scissors"]; 
